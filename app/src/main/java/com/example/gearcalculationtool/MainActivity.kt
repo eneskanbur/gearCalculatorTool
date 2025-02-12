f4eaf211336b11814a1e0b22acc9c5e0009d1ddd
Enes Kanbur <eneskanbur02@hotmail.com>
2025-02-12 16:16:58 +0300

    navigation structure adjusted

--- app/src/main/java/com/example/gearcalculationtool/MainActivity.kt
+++ app/src/main/java/com/example/gearcalculationtool/MainActivity.kt
@@ -5,11 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationBar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationBar.setupWithNavController(navHostFragment.navController)
     }
 }
